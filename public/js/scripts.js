6a250293f6cc2dff8b021ab61567e11e13f29aa1
rloading <r.loading@gmail.com>
2021-08-01 19:49:07 -0300

    feat: side menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -30,3 +30,13 @@ function sendEmail() {
     }
 }
 
+//---- Menu lateral ----//
+
+let btnMenu = document.getElementById("btnMenu")
+let nav = document.getElementById("nav")
+
+btnMenu.addEventListener("click",() => {
+    nav.classList.toggle("active");
+    btnMenu.classList.toggle("active");
+});
+
